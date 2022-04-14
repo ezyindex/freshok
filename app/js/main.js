26b1ff07e11ea15bb1d3496a34506c5ba7c215dd
ezyindex <nikita.pashchenko25755@gmail.com>
2022-04-14 15:27:22 +0300

    added about

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,9 @@
 
 $(function () {
   $('.slider__advertising').slick({
-    dots:true
+  prevArrow: '<button type = "button" class="slick-prev"><img src="images/slider/icon/arrow-left.svg" alt="предодущий слайд"></button>',
+  nextArrow: '<button type="button" class="slick-next"><img src="images/slider/icon/arrow-right.svg" alt="следующий слайд"></button>'
+ 
   });
-$('.single-item').slick();
+
 });
