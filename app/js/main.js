0f805453f0193f62b5183858b040c6d7bc97901b
ezyindex <nikita.pashchenko25755@gmail.com>
2022-04-26 21:44:55 +0300

    13dz done

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,19 @@
-
 $(function () {
   $('.slider-top__advertising').slick({
-  prevArrow: '<button type = "button" class="slider-top__arrow slider-top__arrow--prev"><img src="images/slider/icon/arrow-left.svg" alt="предодущий слайд"></button>',
-  nextArrow: '<button type="button" class="slider-top__arrow slider-top__arrow--next"><img src="images/slider/icon/arrow-right.svg" alt="следующий слайд"></button>'
- 
+    prevArrow: '<button type = "button" class="slider-top__arrow slider-top__arrow--prev"><img src="images/slider/icon/arrow-left.svg" alt="предодущий слайд"></button>',
+    nextArrow: '<button type="button" class="slider-top__arrow slider-top__arrow--next"><img src="images/slider/icon/arrow-right.svg" alt="следующий слайд"></button>'
+
+  });
+
+  $('.slider-bot__list').slick({
+    arrows: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000
   });
 
 });
 
 var mixer = mixitup('.top-product__list');
+var mixer = mixitup('.sales__list');
