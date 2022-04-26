45b9936442470ab16760e3c519e218ff8eec7130
ezyindex <nikita.pashchenko25755@gmail.com>
2022-04-26 16:09:35 +0300

    fixed issuses

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,11 @@
 
 $(function () {
-  $('.slider__advertising').slick({
-  prevArrow: '<button type = "button" class="slick-prev"><img src="images/slider/icon/arrow-left.svg" alt="предодущий слайд"></button>',
-  nextArrow: '<button type="button" class="slick-next"><img src="images/slider/icon/arrow-right.svg" alt="следующий слайд"></button>'
+  $('.slider-top__advertising').slick({
+  prevArrow: '<button type = "button" class="slider-top__arrow slider-top__arrow--prev"><img src="images/slider/icon/arrow-left.svg" alt="предодущий слайд"></button>',
+  nextArrow: '<button type="button" class="slider-top__arrow slider-top__arrow--next"><img src="images/slider/icon/arrow-right.svg" alt="следующий слайд"></button>'
  
   });
 
 });
+
+var mixer = mixitup('.top-product__list');
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
-const {src, dest, watch, parallel, series} = require('gulp');
-const scss         = require('gulp-sass')(require('sass'));
-const concat       = require('gulp-concat');
+const {
+  src,
+  dest,
+  watch,
+  parallel,
+  series
+} = require('gulp');
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
-const uglify       = require('gulp-uglify');
-const imagemin     = require('gulp-imagemin');
-const del     = require('del');
-const browserSync  = require('browser-sync').create();
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+const browserSync = require('browser-sync').create();
 
 function browsersync() {
   browserSync.init({
@@ -18,11 +24,13 @@ function browsersync() {
 
 function styles() {
   return src('app/scss/style.scss')
-    .pipe(scss({outputStyle: 'compressed'}))
+    .pipe(scss({
+      outputStyle: 'compressed'
+    }))
     .pipe(concat('style.min.css'))
     .pipe(autoprefixer({
-      overrideBrowserslist: ['last 10 versions'], 
-      grid:true
+      overrideBrowserslist: ['last 10 versions'],
+      grid: true
     }))
     .pipe(dest('app/css'))
     .pipe(browserSync.stream())
@@ -30,20 +38,21 @@ function styles() {
 
 function scripts() {
   return src([
-    'node_modules/jquery/dist/jquery.js',
-    'node_modules/slick-carousel/slick/slick.js',
-    'app/js/main.js'
-  ])
-  .pipe(concat('main.min.js'))
-  .pipe(uglify())
-  .pipe(dest('app/js'))
-  .pipe(browserSync.stream())
+      'node_modules/jquery/dist/jquery.js',
+      'node_modules/slick-carousel/slick/slick.js',
+      'node_modules/mixitup/dist/mixitup.js',
+      'app/js/main.js'
+    ])
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream())
 }
 
 function images() {
   return src('app/images/**/*.*')
-  .pipe(imagemin([
-    imagemin.gifsicle({
+    .pipe(imagemin([
+      imagemin.gifsicle({
         interlaced: true
       }),
       imagemin.mozjpeg({
@@ -55,24 +64,26 @@ function images() {
       }),
       imagemin.svgo({
         plugins: [{
-            removeViewBox:true
+            removeViewBox: true
           },
           {
             cleanupIDs: false
           }
         ]
       })
-  ]))
-  .pipe(dest('dist/images'))
+    ]))
+    .pipe(dest('dist/images'))
 }
 
 function build() {
   return src([
-    'app/**/*.html',
-    'app/css/style.min.css',
-    'app/js/main.min.js'
-  ], {base:'app'})
-  .pipe(dest('dist'))
+      'app/**/*.html',
+      'app/css/style.min.css',
+      'app/js/main.min.js'
+    ], {
+      base: 'app'
+    })
+    .pipe(dest('dist'))
 }
 
 function cleanDist() {
@@ -93,4 +104,3 @@ exports.images = images;
 exports.cleanDist = cleanDist;
 exports.build = series(cleanDist, images, build);
 exports.default = parallel(styles, scripts, browsersync, watching);
-
